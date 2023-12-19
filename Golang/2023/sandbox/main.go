06370b6f5cbbad6388082ab3f79f398177b243a3
Alexander Markov <markovalex2004@gmail.com>
2023-12-19 23:10:21 +0300

    mock

---
+++ Golang/2023/sandbox/main.go
@@ -0,0 +1,16 @@
+package main
+
+//go:generate mockgen -source=main.go -destination=mocks/mock.go
+
+type addition interface{
+	
+}
+
+func sum(num ...int) int {
+	ans := 0
+	return ans
+}
+
+func main() {
+
+}
